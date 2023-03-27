6b181b2f5eb8f115fa7662641b717bf6e5162a64
4xMafole <4cruxt.unknown@gmail.com>
2023-03-27 22:43:54 +0300

    fix: removed default counter

--- test/app/view/app_test.dart
+++ test/app/view/app_test.dart
@@ -1,12 +1,1 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:islamic_ai/app/app.dart';
-import 'package:islamic_ai/counter/counter.dart';
-
-void main() {
-  group('App', () {
-    testWidgets('renders CounterPage', (tester) async {
-      await tester.pumpWidget(const App());
-      expect(find.byType(CounterPage), findsOneWidget);
-    });
-  });
-}
+void main() {}
